125104622e174e5e593cd5152d9411f8fcf342c4
zemira kehati <kehati10@gmail.com>
2022-09-20 11:17:14 +0300

    send email by emailjs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,37 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 export const Contact = () => {
+  const form = useRef();
+
+  const resetForm = (e) => {
+    e.target.reset();
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_q47y34h",
+        "contact_form",
+        form.current,
+        "lpQc1P_XPQXoEiJxi",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      )
+      .catch((error) => {
+        console.log(error.text);
+      })
+      .finally(resetForm(e));
+  };
+
   return (
     <section className="container contact__container" id="contact">
       <div className="inner__container">
@@ -7,38 +40,43 @@ export const Contact = () => {
           <p className="contact__Subtitle">Drop me a line.</p>
           <p className="contact__Subtitle">I would like to hear from you.</p>
         </div>
-        <form className="contact__form" name="form" noValidate>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="contact__form"
+          name="form"
+          noValidate
+        >
           <fieldset className="contact__form-formset">
             <input
               className="contact__form-input"
               type="text"
               placeholder="Name"
+              name="user_name"
               required
             />
             <input
               className="contact__form-input"
               type="Email"
               placeholder="your_email@mail.com"
+              name="user_email"
               required
             />
             <textarea
               className="contact__form-textarea"
               placeholder="Message"
+              name="message"
             ></textarea>
             <button
               className="button contact__button"
               aria-label="submit"
-              type="button"
+              type="submit"
             >
               Send Message
             </button>
           </fieldset>
         </form>
       </div>
-      {/* <nav class="footer__column footer__column_type_contact">
-        <h2 class="footer__column-heading">Email us</h2>
-        
-      </nav> */}
     </section>
   );
 };
