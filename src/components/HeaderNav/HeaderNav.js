f899a4ffa9458576c40a569602380e9e6b2ef6db
zemira kehati <kehati10@gmail.com>
2022-09-20 11:15:11 +0300

    update header nav

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,36 +1,57 @@
-import React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
+const navList = [
+  {
+    _id: 1,
+    link: "#home",
+    text: "Home",
+  },
+  {
+    _id: 2,
+    link: "#about",
+    text: "About",
+  },
+  {
+    _id: 3,
+    link: "#skills",
+    text: "Skills",
+  },
+  {
+    _id: 4,
+    link: "#projects",
+    text: "Projects",
+  },
+  {
+    _id: 5,
+    link: "#contact",
+    text: "Contact",
+  },
+];
 
 export const HeaderNav = () => {
+  const [toogle, setToggle] = useState(false);
+  const toggleNav = () => {
+    setToggle(!toogle);
+  };
   return (
     <>
-      <GiHamburgerMenu className="header__burger" />
-      <nav className="nav__container">
-        <li className="list__item">
-          <a className="nav__item" href="#home">
-            Home
-          </a>
-        </li>
-        <li className="list__item">
-          <a className="nav__item" href="#about">
-            About
-          </a>
-        </li>
-        <li className="list__item">
-          <a className="nav__item" href="#skills">
-            Skills
-          </a>
-        </li>
-        <li className="list__item">
-          <a className="nav__item" href="#projects">
-            Projects
-          </a>
-        </li>
-        <li className="list__item">
-          <a className="nav__item" href="#contact">
-            Contact
-          </a>
-        </li>
+      <nav className="nav">
+        <button className="nav__mobile-button" onClick={toggleNav}>
+          <span className="nav__icon"></span>
+        </button>
+        <ul className={toogle ? "nav__container nav_opened" : "nav__container"}>
+          {navList.map((item) => (
+            <li key={item._id} className="nav__item">
+              <a
+                className="nav__link nav__link_type_subscribe"
+                href={item.link}
+                onClick={toggleNav}
+              >
+                {item.text}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </>
   );
